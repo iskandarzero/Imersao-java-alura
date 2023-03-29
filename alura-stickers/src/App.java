223705972554a1042e8b76ac580ffed3ef135d67
iskandarzero <riderzerotg@gmail.com>
2023-03-29 06:49:53 -0300

    Aula 2

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -16,8 +18,12 @@ public class App {
         String body = response.body();
         var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        GeradoraDeFigurinhas geradora = new GeradoraDeFigurinhas();
         
         for (Map<String, String> filme : listaDeFilmes) {
+            var urlImagem = filme.get("image");
+            var title = filme.get("title");
+            var nomeDoArquivo = title + ".png";
             var rating = Integer.parseInt(filme.get("imDbRating").substring(0, 1));
             var stars = "";
 
@@ -25,9 +31,11 @@ public class App {
                 stars += "\u2B50";
             }
 
+            InputStream inputStream = new URL(urlImagem).openStream();
+            geradora.cria(inputStream, nomeDoArquivo);
+
             System.out.println(stars);
-            System.out.println("\u001b[37;1m \u001b[44;1m" + filme.get("title") + "\u001b[m");
-            System.out.println(filme.get("image"));
+            System.out.println("\u001b[37;1m \u001b[44;1m" + title + "\u001b[m");
             System.out.println();
         }
     }
