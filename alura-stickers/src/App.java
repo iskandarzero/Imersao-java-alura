48a4dcd66da730d363a433eccf2b1aa35bae9a6b
iskandarzero <riderzerotg@gmail.com>
2023-03-31 06:18:41 -0300

    aula3

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -1,41 +1,23 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
-import java.util.List;
-import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        URI address = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(address).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
-        var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        var http = new ClienteHTTP();
+        String body = http.buscaDados(url);
         GeradoraDeFigurinhas geradora = new GeradoraDeFigurinhas();
+        var extratorIMDB = new ExtratorIMDB();
+        var conteudos = extratorIMDB.extraiConteudos(body);
         
-        for (Map<String, String> filme : listaDeFilmes) {
-            var urlImagem = filme.get("image");
-            var title = filme.get("title");
-            var nomeDoArquivo = title + ".png";
-            var rating = Integer.parseInt(filme.get("imDbRating").substring(0, 1));
-            var stars = "";
+        for (int i = 0; i < 10; i++) {
+            Conteudo conteudo = conteudos.get(i);
 
-            for (int i = 0; i < rating; i++) {
-                stars += "\u2B50";
-            }
-
-            InputStream inputStream = new URL(urlImagem).openStream();
+            var nomeDoArquivo = conteudo.getTitulo() + ".png";
+            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
             geradora.cria(inputStream, nomeDoArquivo);
 
-            System.out.println(stars);
-            System.out.println("\u001b[37;1m \u001b[44;1m" + title + "\u001b[m");
+            System.out.println(conteudo.getTitulo());
             System.out.println();
         }
     }
